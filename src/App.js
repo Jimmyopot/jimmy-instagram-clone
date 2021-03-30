6f4390a662bf319769c9e0ffb50c6293fb888ee1
Jimmyopot <jimmyopot@gmail.com>
2021-03-30 17:11:48 +0300

    Working on the IG component posts

--- src/App.js
+++ src/App.js
@@ -3,6 +3,24 @@ import './App.css';
 import Post from './components/Post';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "Greyfish",
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs9fSoqm0D_uSd1MdxKNC-qiEGu8vl0ixQMA&usqp=CAU",
+      caption: "We'll be posting about crazy animals"
+    },
+    {
+      username: "Sandra",
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3F0U5I8ie-oif5GAgiAwetraqaIivk5Xcvw&usqp=CAU",
+      caption: "Check out this crazy monkey, or ape like..."
+    },
+    {
+      username: "John",
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE3zvk59wehAUA9YonMTZIVyGPl-nEYjYl9g&usqp=CAU",
+      caption: "Crazy see creature"
+    },
+  ]);
+
   return (
     <div className="App">
       
@@ -17,8 +35,33 @@ function App() {
 
       <h1>Jimmy's IG Clone</h1>
 
+      {/* Loop through the posts */}
+      {
+        posts.map(post => (
+          <Post 
+            username={post.username}
+            imageUrl={post.imageUrl} 
+            caption={post.caption}
+          />
+        ))
+      }
+
       {/* Posts */}
-      <Post />
+      {/* <Post 
+          username="Greyfish"
+          imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs9fSoqm0D_uSd1MdxKNC-qiEGu8vl0ixQMA&usqp=CAU" 
+          caption="We'll be posting about crazy animals"
+        />
+      <Post 
+          username="Sandra"
+          imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3F0U5I8ie-oif5GAgiAwetraqaIivk5Xcvw&usqp=CAU" 
+          caption="Check out this crazy monkey, or ape like..."
+      />
+      <Post 
+          username="John"
+          imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE3zvk59wehAUA9YonMTZIVyGPl-nEYjYl9g&usqp=CAU" 
+          caption="Crazy see creature"
+      /> */}
 
     </div>
   );
--- src/components/Post.js
+++ src/components/Post.js
@@ -2,30 +2,24 @@ import React from 'react';
 import './Post.css';
 import Avatar from '@material-ui/core/Avatar';
 
-function Post() {
+function Post({ username, imageUrl, caption }) {
     return (
         <div className="post">
             {/* header -> avatar -> username */}
             <div className="post__header">
-                fff
-
+                <Avatar  
+                    className="post__avatar"
+                    alt="Sparta"
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj9e6zyfiyzACmHMalxDaX8GNXxnF6YIyz3w&usqp=CAU"
+                />
+            <h3>{username}</h3>
             </div>
-            <Avatar 
-            hhhh
-            jjjjj
-            hhhh
-            
-                className="post__avatar"
-                alt="Sparta"
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj9e6zyfiyzACmHMalxDaX8GNXxnF6YIyz3w&usqp=CAU"
-            />
-            <h3>Username</h3>
             <img 
                 className="post__image"
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs9fSoqm0D_uSd1MdxKNC-qiEGu8vl0ixQMA&usqp=CAU" 
-                alt="" 
+                src={imageUrl} 
+                alt="cats" 
             />
-            <h4 className="post__text"><strong>Greyfish</strong> We'll be posting about crazy animals</h4>
+            <h4 className="post__text"><strong>{username}</strong>: {caption}</h4>
 
         </div>
     )
